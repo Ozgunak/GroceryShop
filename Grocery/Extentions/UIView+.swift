7669db65233aca7dd0aea4025e5686d5b269ecd9
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2022-02-21 18:22:53 +0300

    Add images

---
+++ Grocery/Extentions/UIView+.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+.swift
+//  Grocery
+//
+//  Created by ozgun on 20.02.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius : CGFloat{
+        get { return self.cornerRadius}
+        set { self.layer.cornerRadius = newValue}
+    }
+    func addShadowAndCornerRadius() {
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize.zero
+        layer.shadowColor = UIColor.darkGray.cgColor
+    }
+}
